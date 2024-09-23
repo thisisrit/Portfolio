55e5ac72aa0c688468b2cdf5abbdde3b43e03c83
thisisrit <ritrai1998@gmail.com>
2024-09-23 12:28:38 +0530

    updated some codes

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,7 +6,7 @@ const Intro = () => {
     return (
         <div className="flex gap-14 flex-col-reverse lg:flex-row sm:flex-row justify-between items-center w-full">
             <div className="flex flex-col gap-4 md:w-9/12 sm:w-full">
-                <h1 className="text-4xl font-bold">Hi! I'm Rit.</h1>
+                <h1 className="text-4xl font-bold">Hi! I am Rit.</h1>
                 <p className="text-base text-gray-700 dark:text-gray-300">
                     A dedicated Network Support Engineer with 1+ years of experience in troubleshooting, maintaining, and optimizing network infrastructure. Recently, expanded my technical expertise by developing skills in MERN (MongoDB, Express, React, Node.js) stack development, with a strong interest in transitioning into a full-stack developer role. I am passionate about building dynamic and scalable web applications. While I am eager to advance my career in MERN stack development, I remain open to opportunities in network support where I can leverage my problem-solving skills and technical knowledge.
                 </p>
